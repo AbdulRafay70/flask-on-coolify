d91b95a8bb82f0c3d6a127d18dd589bba5b96453
Abdul Rafay <bscs.2023.070@student.gcuh.edu.pk>
2025-07-21 06:58:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Flask App running on Coolify with a custom domain!"
+    return "It works!"
+
+if __name__ == "__main__":
+    app.run()
